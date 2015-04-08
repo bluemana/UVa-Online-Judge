c059c3df54f9c6ed0bf26c0110db89e8acd5b4b2
Giulio Meneghin <bluemana@gmail.com>
2015-04-08 19:03:44 +0200

    Refactored solution of problem 11172

--- src/main/java/uvaoj/volume111/P11172.java
+++ src/main/java/uvaoj/volume111/P11172.java
@@ -1,33 +1,59 @@
 package uvaoj.volume111;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		reader.readLine();
-		String line;
-		while ((line = reader.readLine()) != null && !line.isEmpty()) {
-			String[] tokens = line.split(" ");
-			int i1 = Integer.parseInt(tokens[0]);
-			int i2 = Integer.parseInt(tokens[1]);
-			String result = P11172.solve(i1, i2);
-			System.out.println(result);
-		}
+		Writer writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		P11172.solve(reader, writer);
 	}
 }
 
 class P11172 {
 	
-	public static String solve(int i1, int i2) {
-		String result = "=";
+	public static void solve(BufferedReader reader, Writer writer) throws Exception {
+		reader.readLine(); // skip first line
+		String line;
+		while ((line = reader.readLine()) != null && !line.isEmpty()) {
+			String[] tokens = line.split(" ");
+			int i1 = Integer.parseInt(tokens[0]);
+			int i2 = Integer.parseInt(tokens[1]);
+			Relation result = P11172.compare(i1, i2);
+			writer.write(result + "\n");
+		}
+	}
+	
+	private static Relation compare(int i1, int i2) {
+		Relation result = Relation.EQUAL;
 		if (i1 < i2) {
-			result = "<";
+			result = Relation.LESS_THAN;
 		} else if (i1 > i2) {
-			result = ">";
+			result = Relation.GREATER_THAN;
 		}
 		return result;
+	}
+}
+
+enum Relation {
+	
+	EQUAL("="),
+	LESS_THAN("<"),
+	GREATER_THAN(">");
+	
+	private final String string;
+	
+	private Relation(String string) {
+		this.string = string;
+	}
+	
+	@Override
+	public String toString() {
+		return string;
 	}
 }
--- src/test/java/uvaoj/volume111/P11172Test.java
+++ src/test/java/uvaoj/volume111/P11172Test.java
@@ -1,22 +1,29 @@
 package uvaoj.volume111;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class P11172Test {
 
 	@Test
-	public void testExample() {
-		String[] actualResults = new String[] {
-			P11172.solve(10, 20),
-			P11172.solve(20, 10),
-			P11172.solve(10, 10)
-		};
-		String[] expectedResults = new String[] {
-			"<",
-			">",
-			"="
-		};
-		Assert.assertArrayEquals(expectedResults, actualResults);
+	public void testExample() throws Exception {
+		String input =
+				"3\n" +
+				"10 20\n" +
+				"20 10\n" +
+				"10 10\n";
+		String expectedOutput =
+				"<\n" +
+				">\n" +
+				"=\n";
+		BufferedReader reader = new BufferedReader(new StringReader(input));
+		Writer writer = new StringWriter();
+		P11172.solve(reader, writer);
+		Assert.assertArrayEquals(new String[] {expectedOutput}, new String[] {writer.toString()});
 	}
 }
